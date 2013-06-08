f9c3bd1486a53f7c26137b7df1e083e05d4fb1a5
Makoto Inoue <inouemak@googlemail.com>
2013-06-08 20:07:22 +0100

    Removed unused code

--- js/carousel_view.js
+++ js/carousel_view.js
@@ -4,14 +4,9 @@ var CarouselView = Backbone.View.extend({
     var template = $('.carousel-template').text()
     $('.carousel').html(Mustache.render(template, {contents:this.collection.toJSON()}))
     $('.carousel').carousel('next')
-    $('.carousel').carousel('stop')
   },
   initialize: function(collection){
     this.collection = collection
     this.render()
-  },
-  open: function(e){
-    e.preventDefault();
-    var tag = e.target.text
   }
 });
--- js/navigation_tags_view.js
+++ js/navigation_tags_view.js
@@ -4,9 +4,7 @@ var NavigationTagsView = Backbone.View.extend({
   render: function() {
     var template = $('.navigation-tags-template').text()
     $('.navigation-tags').html(Mustache.render(template, {tags:this.tags}))
-    console.log('template', template)
     // This should go into events:{}
-    console.log('aaa', this.tags)
     $('.navigation-tags a').on('click', this.open)
   },
   initialize: function(tags){
@@ -16,7 +14,7 @@ var NavigationTagsView = Backbone.View.extend({
   open: function(e){
     e.preventDefault();
     var tag = e.target.text
-    console.log('oepn', window.contents)
     new CarouselView(window.contents.filterByTag(tag))
+    new ThumnailsView(window.conten)
   }
 });
